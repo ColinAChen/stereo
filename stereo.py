b6dfc013a6e24daf269367cda7f8786c53945551
Colin Chen <colin.a.chen@gmail.com>
2019-09-04 19:12:56 -0700

    library tests

--- stereo.py
+++ stereo.py
@@ -1,4 +1,6 @@
 import cv2
+import matplotlib.pyplot as plt
+from skimage.draw import line
 #Real world cm height/width
 realWidth = .86808
 realHeight = .65106
@@ -24,6 +26,22 @@ def realToPixel(cmx,cmy):
 	y = (cmy + focalY)*480/realHeight
 	return (x,y)
 
+def match(img1,x,y,img2,linex1,liney1,linex2,liney2):
+	diff = 255
+	matchRow = None
+	matchCol = None
+	rr,cc = line(linex1,liney1,linex2,liney2)
+	for row in rr,cc:
+		for col in row:
+			if(abs(img1[x][y] - img2[row][col]) < diff):
+				diff = abs(img1[x][y] - img2[row][col]) < diff
+				matchRow = row
+				matchCol = col
+	return matchRow,matchCol
+
+
+
+#def realDistance()
 
 def main():
 	cap0 = cv2.VideoCapture(0)
@@ -36,20 +54,49 @@ def main():
 	#cap0.set(4,1080)
 	#print(cap0.get(3))
 	#print(cap0.get(4))
+	#stereo = cv2.StereoBM_create(0,21)
 	while(True):
 		# Capture frame-by-frame
 		#ret, frame = cap.read()
 		ret0, frame0 = cap0.read()
 		ret1, frame1 = cap1.read()
 		# Our operations on the frame come here
-		#gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-		#gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+		gray0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
+		gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+		for row in gray0:
+			for col in row:
+				matchX, matchY = match(row,col)
 
+
+
+
+		#orb = cv2.ORB_create()
+
+		#kp1, des1 = orb.detectAndCompute(gray0,None)
+		#kp2, des2 = orb.detectAndCompute(gray1,None)
+
+		# create BFMatcher object
+		#bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+		# Match descriptors.
+		#matches = bf.match(des1,des2)
+
+		# Sort them in the order of their distance.
+		#matches = sorted(matches, key = lambda x:x.distance)
+		#print(des1[matches[0].trainIdx])
+		# Draw first 10 matches.
+		#img3 = cv2.drawMatches(gray0,kp1,gray1,kp2,matches[:50],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+		#cv2.imshow('matches',img3)
+		#plt.imshow(img3),plt.show()
+		#out=[]
+		#disparity = stereo.compute(gray0,gray1)
+		#retval = stereo.getMinDisparity()
+		#print(retval)
+		#print(disparity)
 		#ret0,thresh0 = cv2.threshold(gray0,250,255,cv2.THRESH_BINARY)
 		#ret1,thresh1 = cv2.threshold(gray1,250,255,cv2.THRESH_BINARY)
 		# Display the resulting frame
-		cv2.imshow('frame',frame0)
-		cv2.imshow('frame1',frame1)
+		#cv2.imshow('frame',frame0)
+		#cv2.imshow('frame1',frame1)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
